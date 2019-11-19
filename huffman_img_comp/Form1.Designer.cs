8beb4abe415faf787cb251d660183b7f94f01edf
PrabakarSiva <46516495+PrabakarSiva@users.noreply.github.com>
2019-11-18 21:30:16 -0800

    first commit

---
+++ huffman_img_comp/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace huffman_img_comp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(308, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Drag an image into the panel below.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(293, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(214, 134);
+            this.panel1.TabIndex = 3;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(490, 266);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(214, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Drag an encoded text file into the panel below.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(487, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Decoded Image";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Decode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(83, 229);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(224, 96);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(82, 344);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(223, 96);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Drag the tree text file into the panel below.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ huffman_img_comp/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace huffman_img_comp
+{
+    public partial class Form1 : Form
+    {
+        bool vdata;
+        string path;
+        Image image;
+        Thread ImageThread;
+        int[] intRepBytes;
+        byte[] bytes;
+        BitArray encoded;
+        HuffmanTree huffmanTree = new HuffmanTree();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            richTextBox1.AllowDrop = true;
+            richTextBox1.DragDrop += RichTextBox1_DragDrop;
+
+            richTextBox2.AllowDrop = true;
+            richTextBox2.DragDrop += RichTextBox2_DragDrop;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Prompt to get file names in order to save encoded data to a file
+            string promptValue = Prompt.ShowDialog("Please enter the name you would like to use for the generated text file.", "Filename Prompt");
+            string textfilePath = Path.Combine(Environment.CurrentDirectory, @"Data\", promptValue);
+
+            // Build the Huffman tree
+            huffmanTree.Build(intRepBytes);
+
+            // Encode
+            encoded = huffmanTree.Encode(intRepBytes);
+
+            // Saving to file and printing out for console output
+            byte[] encodedData = new byte[encoded.Length / 8 + (encoded.Length % 8 == 0 ? 0 : 1)];
+            encoded.CopyTo(bytes, 0);
+            File.WriteAllBytes(textfilePath + ".bin", encodedData);
+
+            Console.Write("Encoded Bistream: ");
+            foreach (bool bit in encoded)
+            {
+                Console.Write((bit ? 1 : 0) + "");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Encoded bitstream is " + encoded.Length + " bits long.");
+
+            // Saving the tree so it can be decoded later 
+            promptValue += "Tree";
+            textfilePath = Path.Combine(Environment.CurrentDirectory, @"Data\", promptValue);
+            File.WriteAllBytes(textfilePath + ".bin", bytes);
+
+            
+
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            string filename;
+            vdata = GetImage(out filename, e);
+            if (vdata)
+            {
+                path = filename;
+                ImageThread = new Thread(new ThreadStart(saveimage));
+                ImageThread.Start();
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void saveimage()
+        {
+            //throw new NotImplementedException();
+            image = new Bitmap(path);
+        }
+
+        private bool GetImage(out string filename, DragEventArgs e)
+        {
+            //throw new NotImplementedException();
+            bool rtun = false;
+            filename = string.Empty;
+            if ((e.AllowedEffect & DragDropEffects.Copy) == DragDropEffects.Copy)
+            {
+                Array data = ((IDataObject)e.Data).GetData("FileDrop") as Array;
+                if (data != null)
+                {
+                    if ((data.Length == 1) && (data.GetValue(0) is string))
+                    {
+                        filename = ((string[])data)[0];
+                        string extension = Path.GetExtension(filename).ToLower();
+                        if ((extension == ".jpg") || (extension == ".png") || (extension == ".gif") || (extension == ".bmp"))
+                        {
+                            rtun = true;
+                        }
+                    }
+                }
+            }
+            return rtun;
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            // Displaying the image on the panel and returning a bitmapped byte stream of the image
+            if (vdata)
+            {
+                while (ImageThread.IsAlive)
+                {
+                    Application.DoEvents();
+                    Thread.Sleep(0);
+                }
+                panel1.BackgroundImage = image;
+                var ms = new MemoryStream();
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                bytes = ms.ToArray();
+                intRepBytes = new int[bytes.Length];
+                Console.Write("Byte-Representation of Image: ");
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    int x = 0;
+                    Int32.TryParse(bytes.GetValue(i).ToString(), out x);
+                    intRepBytes.SetValue(x, i);
+                    Console.Write(intRepBytes.GetValue(i) + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Total number of bytes in the image: " + bytes.Length);
+                Console.WriteLine("Total number of bits in the image: " + bytes.Length * 8);
+
+            }
+        }
+
+        public Image byteArrayToImage(byte[] bytesArr)
+        {
+            using (MemoryStream memstr = new MemoryStream(bytesArr))
+            {
+                Image img = Image.FromStream(memstr);
+                return img;
+            }
+        }
+
+        public string ToBitString(BitArray bits)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < bits.Count; i++)
+            {
+                char c = bits[i] ? '1' : '0';
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            string decoded = huffmanTree.Decode(encoded);
+            //Console.WriteLine(decoded);
+            //byte[] decodedBytes = Encoding.UTF8.GetBytes(decoded);
+            //ArrayList decodeList = new ArrayList();
+            //Console.WriteLine("hello");
+            //String temp = "";
+            Console.WriteLine(decoded.Split().Length - 1);
+            int[] decodeArray = new int[decoded.Split().Length - 1];
+            String temp = "";
+            int counter = 0;
+            foreach (char c in decoded)
+            {
+                if (Char.IsWhiteSpace(c))
+                {
+                    int placeholder = 0;
+                    Int32.TryParse(temp, out placeholder);
+                    decodeArray.SetValue(placeholder, counter);
+                    counter++;
+                    temp = "";
+                }
+                else
+                {
+                    temp += c;
+                }
+            }
+            foreach (var item in decodeArray)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+            Console.WriteLine();
+            byte[] bytes2 = decodeArray.Select(i => (byte)i).ToArray();
+            Console.WriteLine(bytes2.Length);
+            var imageMemoryStream = new MemoryStream(bytes);
+            Image imgFromStream = Image.FromStream(imageMemoryStream);
+            pictureBox1.Image = imgFromStream;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.AllowDrop = true;
+            richTextBox1.DragDrop += RichTextBox1_DragDrop;
+        }
+
+        private void RichTextBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if (data != null)
+            {
+                var fileNames = data as string[];
+                if (fileNames.Length > 0)
+                {
+                    richTextBox1.LoadFile(fileNames[0]);
+                }
+
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox2.AllowDrop = true;
+            richTextBox2.DragDrop += RichTextBox2_DragDrop;
+        }
+
+        private void RichTextBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if (data != null)
+            {
+                var fileNames = data as string[];
+                if (fileNames.Length > 0)
+                {
+                    richTextBox2.LoadFile(fileNames[0]);
+                }
+
+            }
+        }
+    }
+
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Width = 400, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 40, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+    }
+}
